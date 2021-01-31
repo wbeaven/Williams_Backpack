6ed87a16b302f153daef3ca44d8056c739f72d3c
wbeaven <wtbeaven@gmail.com>
2021-02-01 01:29:05 +1100

    Add Skele Punch unity project

---
+++ Unity Projects/Skele Punch/Assets/Scripts/SkeleSpawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SkeleSpawn : MonoBehaviour
+{
+    public GameObject foam;
+    public float speed = 10f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject instFoam = Instantiate(foam, transform.position, Quaternion.identity);
+            Rigidbody instFoamRB = instFoam.GetComponent<Rigidbody>();
+
+            instFoamRB.AddForce(gameObject.transform.forward * speed);
+            Destroy(instFoam, 3f);
+        }
+    }
+}
